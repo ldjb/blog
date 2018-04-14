c70fe496ce05f32107226dce177f40d597647b44
Leon Byford <leon@ldjb.zendesk.com>
2018-04-15 00:13:13 +0100

    refactor future script into include

---
+++ _includes/future.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".future").forEach(
+	post => {post.dataset.pubdate <= new Date().toISOString()
+		&& (post.style.display = "{{include.display|default:"list-item"}}");}
+);
+var lastTenKeys = [null, null, null, null, null, null, null, null, null, null];
+var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+addEventListener("keydown", e => {
+	lastTenKeys.shift();
+	lastTenKeys.push(e.key);
+	if (JSON.stringify(lastTenKeys) == JSON.stringify(konamiCode)) {
+		document.querySelectorAll(".future").forEach(
+			post => {post.style.display = "{{include.display|default:"list-item"}}";}
+		);
+	}
+});
