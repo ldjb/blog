ecdcb0fd7696d4fb90ade04be52bab70083d07d6
Leon Byford <leon@ldjb.zendesk.com>
2018-04-16 17:23:11 +0100

    fix future and replace fun

--- _includes/future.js
+++ _includes/future.js
@@ -1,15 +1,6 @@
+<script>
 document.querySelectorAll(".future").forEach(
 	post => {post.dataset.pubdate <= new Date().toISOString()
-		&& (post.style.display = "{{include.display|default:"list-item"}}");}
+		&& (post.style.display = "{{include.display|default:"block"}}");}
 );
-var lastTenKeys = [null, null, null, null, null, null, null, null, null, null];
-var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
-addEventListener("keydown", e => {
-	lastTenKeys.shift();
-	lastTenKeys.push(e.key);
-	if (JSON.stringify(lastTenKeys) == JSON.stringify(konamiCode)) {
-		document.querySelectorAll(".future").forEach(
-			post => {post.style.display = "{{include.display|default:"block"}}";}
-		);
-	}
-});
+</script>
